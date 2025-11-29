3f1538c0f2bbc22da7bf5145eb480489b49d258b
Wilmar Arbey Martinez Quintana <wmartinezq@unal.edu.co>
2025-11-29 11:53:39 -0500

    Carga de funciones y pruebas de test

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,53 @@
+# Ejemplo para casos de uso
+# python3 -m homework data/input data/output
 
+import argparse
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Count Word in files.")
+    parser.add_argument(
+        "input", type=str, help="Path to the input folder containing files to process."
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder containing files to process.",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+
+def main():
+    input, output = parse_args()
+    print(f"input: {input}")
+    print(f"output: {output}")
+
+
+# import argparse
+# import sys
+
+
+# def parse_args():
+#    parser = argparse.ArgumentParser(description="Count Word in files.")
+#    parser.add_argument(
+#        "input", type=str, help="Path to the input folder containing files to process."
+#    )
+#    parser.add_argument(
+#        "output",
+#        type=str,
+#        help="Path to the output folder containing files to process.",
+#    )
+
+#    parsed_args = parser.parse_args()
+
+#    return parsed_args.input, parsed_args.output
+
+
+# def main():
+#    input, output = parse_args()
+#    print(f"input: {input}")
+#    print(f"output: {output}")
