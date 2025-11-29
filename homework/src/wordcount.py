3bdde3679b4510a869bdee5bfea387f22585d0fa
Wilmar Arbey Martinez Quintana <wmartinezq@unal.edu.co>
2025-11-29 12:42:50 -0500

    Se crean nuevas funciones de test

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,8 +1,12 @@
+## Wordcount
+
 # Ejemplo para casos de uso
 # python3 -m homework data/input data/output
 
+
 import argparse
-import sys
+
+from homework.src._internals.read_all_lines import read_all_lines
 
 
 def parse_args():
@@ -21,10 +25,29 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_counts):
+    pass
+
+
 def main():
-    input, output = parse_args()
-    print(f"input: {input}")
-    print(f"output: {output}")
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
 
 
 # import argparse
